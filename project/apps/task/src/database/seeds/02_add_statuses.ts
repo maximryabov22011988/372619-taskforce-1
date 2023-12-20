00729fab875c7d3104494a96950bbe31bad5a788
Ryabov Maxim <ryabovms@altarix.ru>
2023-12-20 20:42:31 +0300

    feat: Добавление eslint-правила prefer-node-protocol

--- project/apps/task/src/database/seeds/02_add_statuses.ts
+++ project/apps/task/src/database/seeds/02_add_statuses.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 const tableName = 'statuses';
 
-export async function seed(knex: Knex): Promise<any> {
+export async function seed(knex: Knex): Promise<void> {
   await knex(tableName).insert([
     {
       id: 1,
